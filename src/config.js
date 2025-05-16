908bb58edca7234ba49cd396386b7c70369f8d6b
fernando leiva <fer_bostero_91@hotmail.com>
2025-05-15 22:33:53 -0300

    arreglo en confg se saco el localhost

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 // src/config.js
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://backendcabana.onrender.com' 
-  : 'http://localhost:5000';
+export const API_URL = 'https://backendcabana.onrender.com' 
+  
